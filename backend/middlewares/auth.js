2d8da9b9b97e00c3cc2c1ac67d4d47f3f3e1a294
Alexandre Karneff <alexkarneff@gmail.com>
2021-10-07 18:53:56 +0200

    backend init

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+	try {
+		const token = req.headers.authorization.split(' ')[1];
+		const decodedToken = jwt.verify(token, process.env.TOKEN);
+		const userId = decodedToken.userId;
+		
+		// if (req.body.userId && req.body.userId !== userId) {
+		if (req.body.userId !== userId) {
+			console.log('auth', req.body.userId);
+			console.log('auth', userId);
+			throw 'User Id non valable !';
+		} else {
+			// si tout est ok, on peut passer la requête au prochain middleware
+			next();
+		}
+	} catch (error) {
+		// problème d'authentification
+		res.status(401).json({ error, message: 'Requête non authentifiée' });
+	}
+};
