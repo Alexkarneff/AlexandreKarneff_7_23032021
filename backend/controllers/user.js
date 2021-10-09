e34bc24ce1b015f6dab13b2283493fa6a142f691
Alexandre Karneff <alexkarneff@gmail.com>
2021-10-09 21:28:06 +0200

    Login 2

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,82 +1,80 @@
-const db = require("../models/User");
-const User = db.User;
+const db = require("../models");
+const User = db.user;
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
-const { user } = require("../models");
-
-
 
 exports.signup = (req, res, next) => {
-  console.log(req.body);
-  bcrypt
+    console.log(req.body);
+    bcrypt
     //hash du mot de passe
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      user
-        .create({
-          email: req.body.email,
-          password: hash,
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          admin: req.body.admin,
+        .hash(req.body.password, 10)
+        .then((hash) => {
+            User
+                .create({
+                    email: req.body.email,
+                    password: hash,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    admin: req.body.admin,
+                })
+                .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+                .catch((error) => res.status(400).json({ error }));
         })
-        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => {
-    console.log (error);
-    res.status(500).json({ error })});
+        .catch((error) => {
+            console.log(error);
+            res.status(500).json({ error })
+        });
 };
 
 exports.login = (req, res, next) => {
-  console.log(req.body);
-  User.findOne({ where:{ email: req.body.email} })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({ error: "Utilisateur non trouvé !" });
-      }
-      //on compare le hash pour vérifier le mot de passe
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res.status(401).json({ error: "Mot de passe incorrect !" });
-          }
-          res.status(200).json({
-              user: {
-                id: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: req.body.email,
-                password: req.body.password,
-                description: user.description,
-                admin: user.admin,
-              },
+    console.log(req.body);
+    User.findOne({ where: { email: req.body.email } })
+        .then((user) => {
+            if (!user) {
+                return res.status(401).json({ error: "Utilisateur non trouvé !" });
+            }
+            //on compare le hash pour vérifier le mot de passe
+            bcrypt
+                .compare(req.body.password, user.password)
+                .then((valid) => {
+                    if (!valid) {
+                        return res.status(401).json({ error: "Mot de passe incorrect !" });
+                    }
+                    res.status(200).json({
+                        user: {
+                            id: user.id,
+                            firstName: user.firstName,
+                            lastName: user.lastName,
+                            email: req.body.email,
+                            password: req.body.password,
+                            description: user.description,
+                            admin: user.admin,
+                        },
 
-            token: jwt.sign({ id: user._id }, "RANDOM_TOKEN_SECRET", {
-              expiresIn: "24h",
-            }),
-            message: "Vous êtes connecté!"
-          });
+                        token: jwt.sign({ id: user._id }, "RANDOM_TOKEN_SECRET", {
+                            expiresIn: "24h",
+                        }),
+                        message: "Vous êtes connecté!"
+                    });
+                })
+                .catch((error) => res.status(500).json({ error }));
         })
         .catch((error) => res.status(500).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
 };
 
 exports.deleteAccount = (req, res, next) => {
-  const userId = req.params.id;
+    const userId = req.params.id;
 
-  User.destroy({
-    where: { id: userId },
-  })
-    .then(() => {
-      res.status(200).send({ message: "Suppression du compte effectuée " });
-    })
-    .catch((error) => {
-      res
-        .status(500)
-        .send({ error, message: "Erreur lors de la suppression du compte" });
-    });
+    User.destroy({
+            where: { id: userId },
+        })
+        .then(() => {
+            res.status(200).send({ message: "Suppression du compte effectuée " });
+        })
+        .catch((error) => {
+            res
+                .status(500)
+                .send({ error, message: "Erreur lors de la suppression du compte" });
+        });
 };
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,12 +1,47 @@
+//import { def } from '@vue/shared';
 import { createStore } from 'vuex'
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+
+let state = {
+	userLogged: false,
+	posts: [],
+	user: {},
+	token: '',
+};
+
+const mutations = {
+
+  LOG_USER(state) {
+  state.userLogged = true;
+},
+
+SET_USER_INFO(state, data) {
+  state.user = data;
+},
+
+SET_TOKEN(state, token) {
+  state.token = token;
+},
+
+SET_POSTS(state, posts) {
+  state.posts = posts;
+}
+
+};
+const actions = {
+
+}
+
+const getters = {
+
+}
+
+const store= createStore({
+  state,
+  mutations,
+  actions,
+  getters,
+
+});
+
+export default store;
