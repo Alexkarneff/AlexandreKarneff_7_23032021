a09b4823fe0ed6f9ce425c774cfdff90e508ad95
Alexandre Karneff <alexkarneff@gmail.com>
2021-10-12 13:05:36 +0200

    Comments services

---
+++ frontend/src/services/comments.js
@@ -0,0 +1,34 @@
+// Utilisation d'axios
+import axios from 'axios';
+
+// single axios instance
+const apiClient = axios.create({
+	baseURL: 'http://localhost:3000/api/comments',
+	headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	}
+});
+
+
+// Créer un commentaire
+function createComment(data, token) {
+	return apiClient.post('/', data, 
+		{ headers: {
+			"Authorization": "BEARER " + token
+		}}
+	);
+}
+
+//Supprimer un commentaire
+function deleteComment(id, token) {
+	return apiClient.delete('/' + id, 
+		{ headers: {
+			"Authorization": "BEARER " + token
+		}}
+	);
+}
+
+export default { 
+	createComment, deleteComment
+};
